46d8f959bffe3380d67da2ddd6d7e031ce8dd1d7
chad svastisalee <swasgrafx@gmail.com>
2021-03-04 13:30:43 -0600

    Delete User On Click

--- src/js/users.app.js
+++ src/js/users.app.js
@@ -281,9 +281,9 @@ var usersApp = (function() {
           
                   <div class="text-center">
                     <br>
-                    <a class="btn btn-lg btn-danger text-white">
-                      Yes delete ${data.user.username}
-                    </a>
+                    <a onclick="usersApp.deleteUser('${data.user._id}');" class="btn btn-lg btn-danger text-white">
+                    Yes delete ${data.user.username}
+                  </a>
                   </div>
           
                 </div>
@@ -291,7 +291,33 @@ var usersApp = (function() {
           
               app.innerHTML = card;
             }
-        }            
+        }
+        
+        function deleteUser(id){
+
+            let uri = `${window.location.origin}/api/users/${id}`;
+            let xhr = new XMLHttpRequest();
+            xhr.open('DELETE', uri);
+          
+            xhr.setRequestHeader(
+              'Content-Type',
+              'application/json; charset=UTF-8'
+            );
+          
+            xhr.send();
+          
+            xhr.onload = function(){
+              let data = JSON.parse(xhr.response);
+              if(data.success === true){
+                window.location.hash = '#';
+              }else{
+                alert('Unknown error, the user could not be deleted');
+              }
+          
+            }
+          
+          }
+          
         return {
         load: function(){
             let hash = window.location.hash;
@@ -319,7 +345,12 @@ var usersApp = (function() {
                 viewUsers();
                 break;
             }
-          }
+        },
+
+        deleteUser: function(id){
+          deleteUser(id);
+        }
+        
         }   
   })();
   
